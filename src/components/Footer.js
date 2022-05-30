2c448a341de9b7a19baa594737df90b5120eefe0
victory-nwawunze <victorynwawunze@gmail.com>
2022-05-30 09:13:40 -0700

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,72 +6,55 @@ import Icons from "../images/Group 8110.jpg";
 const Footer = () => {
   return (
     <React.Fragment>
-      <div className="footer-section bg-white p-5">
-        <div className="container-fluid">
-          <div className="grid">
-            <div className="row py-5">
-              <div className="col-md-4">
-                <div className="container">
-                  <div className="row">
-                    <div className="col">
-                      <img src={Logo} className="img-fluid Logo-img  mt-3" />
-                      <p className="text-font mt-3">
-                        © 2021 The Money Africa. All rights reserved.
-                      </p>
-                      <img src={Icons} className="img-fluid Logo-img  mt-3" />
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-3">
-                <div className="container">
-                  <div className="row">
-                    <div className="col">
-                      <h4 className="h4 text-font fw-bold  mt-3">Service</h4>
-                      <p className="text-font  mt-3">MA Premium</p>
-                      <p className="text-font  mt-3">My Learning Platform</p>
-                      <p className="text-font  mt-3">My Kids</p>
-                      <p className="text-font  mt-3">MA Communities</p>
-                      <p className="text-font  mt-3">Digital Advisors</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-2">
-                <div className="container">
-                  <div className="row">
-                    <div className="col">
-                      <h4 className="h4 fw-bold  mt-3">Company</h4>
-                      <p className="text-font  mt-3">About Us</p>
-                      <p className="text-font  mt-3">Careers</p>
-                      <p className="text-font  mt-3">Contact Us</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-1">
-                <div className="container">
-                  <div className="row">
-                    <div className="col">
-                      <h4 className="h4 fw-bold  mt-3">Tools</h4>
-                      <p className="text-font mt-3">Calculator</p>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="col-md-2">
-                <div className="container">
-                  <div className="row">
-                    <div className="col">
-                      <h4 className="h6  mt-3">Sign Up For News Letter</h4>
-                      <small className="font-size mt-3 ">
-                        Sign-up for our newsletter.Your email is never shared.
-                      </small>
-                    </div>
-                  </div>
-                </div>
-              </div>
+      <div className="footer-section p-5 bg-white">
+        <div className="d-flex py-5 justify-content-between align-items-baseline">
+          <div>
+            <img src={Logo} className="img-fluid Logo-img" />
+            <p className="text-font mt-3">
+              © 2021 The Money Africa. All rights reserved.
+            </p>
+            <img src={Icons} className="img-fluid Logo-img mt-3" />
+          </div>
+          <div>
+            <h3 className="h4 text-font fw-bold">Services</h3>
+            <p className="text-font mt-3">MA Premium</p>
+            <p className="text-font mt-3">MA Learning platform </p>
+            <p className="text-font mt-3">MA Kids</p>
+            <p className="text-font mt-3">MA Communities</p>
+            <p className="text-font mt-3">Digital Advisor</p>
+          </div>
+          <div>
+            <h3 className="h4 text-font fw-bold">Company</h3>
+            <p className="text-font mt-3">About Us</p>
+            <p className="text-font mt-3">Careers </p>
+            <p className="text-font mt-3">Contact Us</p>
+          </div>
+          <div>
+            <h3 className="h4 text-font fw-bold">Tools</h3>
+            <p className="text-font mt-3">Calculator</p>
+          </div>
+          <div>
+            <h3 className="h4 text-font fw-bold">Sign up for the newsletter</h3>
+            <small className="text-font mt-3">
+              Sign up for our newsletter. Your email is never
+              <br /> shared.
+              <br />
+              Email
+              <br />
+              Address
+            </small>
+            <div class="input-container">
+              <i class="fa fa-envelope icon fa-2x"></i>
+              <input
+                class="input-field"
+                type="text"
+                placeholder="Email"
+                name="email"
+              />
             </div>
+            <button className="btn btn-danger round text-font">
+              Subscribe
+            </button>
           </div>
         </div>
       </div>
