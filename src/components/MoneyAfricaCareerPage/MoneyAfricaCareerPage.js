ec333b694e1195d1424cfab15b1324eb17165715
victory-nwawunze <victorynwawunze@gmail.com>
2022-06-03 06:25:21 -0700

    creating CareerPage

--- src/components/MoneyAfricaCareerPage/MoneyAfricaCareerPage.js
+++ src/components/MoneyAfricaCareerPage/MoneyAfricaCareerPage.js
@@ -2,6 +2,8 @@
 
 import React from "react";
 import CareerBannerImage from "../../images/Group 8146.png";
+import ReadMoreButton from "../../images/Group 8147.png";
+import LadyBackgroundImage from "../../images/Group 8148.png";
 const MoneyAfricaCareerPage = () => {
   return (
     <React.Fragment>
@@ -47,6 +49,95 @@ const MoneyAfricaCareerPage = () => {
             </div>
           </div>
         </div>
+      </div>
+      <div className="CareerDescriptionText p-5">
+        <p className="text-font text-center fw-bold h1 pt-5">
+          Available positions
+        </p>
+        <div className="container">
+          <div className="grid">
+            <div className="row py-4">
+              <div className="col-md-12 p-3">
+                <div className="card">
+                  <div className="card-body">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div>
+                        <p className="text-font fw-bold">
+                          Marketing specialist
+                        </p>
+                        <small className="text-font SmallTextFont">
+                          Onsite - Lagos, Nigeria
+                        </small>
+                      </div>
+                      <div className="hover">
+                        <img src={ReadMoreButton} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12 p-3">
+                <div className="card">
+                  <div className="card-body">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div>
+                        <p className="text-font fw-bold">Product Designer</p>
+                        <small className="text-font SmallTextFont">
+                          Onsite/Remote - Lagos, Nigeria
+                        </small>
+                      </div>
+                      <div className="hover">
+                        <img src={ReadMoreButton} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12 p-3">
+                <div className="card">
+                  <div className="card-body">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div>
+                        <p className="text-font fw-bold">
+                          Marketing specialist
+                        </p>
+                        <small className="text-font SmallTextFont">
+                          Onsite - Lagos, Nigeria
+                        </small>
+                      </div>
+                      <div className="hover">
+                        <img src={ReadMoreButton} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-12 p-3">
+                <div className="card">
+                  <div className="card-body">
+                    <div className="d-flex justify-content-between align-items-center">
+                      <div>
+                        <p className="text-font fw-bold">
+                          Marketing specialist
+                        </p>
+                        <small className="text-font SmallTextFont">
+                          Onsite - Lagos, Nigeria
+                        </small>
+                      </div>
+                      <div className="hover">
+                        <img src={ReadMoreButton} />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <div className="BackgroundContainerBackgroundImageCareer">
+        <img src={LadyBackgroundImage} className="img-fluid " />
       </div>
     </React.Fragment>
   );
