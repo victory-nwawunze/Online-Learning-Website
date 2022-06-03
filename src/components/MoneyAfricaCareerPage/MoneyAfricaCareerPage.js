152763234354f055d0d2c256b88c2e612ba598f5
victory-nwawunze <victorynwawunze@gmail.com>
2022-06-03 06:50:29 -0700

    creating CareerPage

--- src/components/MoneyAfricaCareerPage/MoneyAfricaCareerPage.js
+++ src/components/MoneyAfricaCareerPage/MoneyAfricaCareerPage.js
@@ -4,6 +4,7 @@ import React from "react";
 import CareerBannerImage from "../../images/Group 8146.png";
 import ReadMoreButton from "../../images/Group 8147.png";
 import LadyBackgroundImage from "../../images/Group 8148.png";
+import Lines from "../../images/Group 8151.png";
 const MoneyAfricaCareerPage = () => {
   return (
     <React.Fragment>
@@ -138,6 +139,55 @@ const MoneyAfricaCareerPage = () => {
 
       <div className="BackgroundContainerBackgroundImageCareer">
         <img src={LadyBackgroundImage} className="img-fluid " />
+      </div>
+
+      <div className="ContainerWithCareerTextDiscription p-4">
+        <div className="container">
+          <div className="grid">
+            <div className="row py-5">
+              <div className="col-md-6">
+                <p className="h3 text-font fw-bold">Team & Process</p>
+                <p className="text-font">
+                  The team mainly comes from an engineering background
+                  <br /> with a strong focus in product development. We like
+                  <br /> difficult challenges and we believe there’s no
+                  technical
+                  <br /> problem we can’t solve.‍
+                </p>
+              </div>
+              <div className="col-md-6">
+                <div className="d-flex align-item-center justify-content-evenly">
+                  <div>
+                    <img src={Lines} />
+                  </div>
+                  <div className="p-1">
+                    <p className="text-font pt-2 h5 fw-bold">
+                      Phone validation
+                    </p>
+                    <p className="text-font ">
+                      We’ll talk about key motivations, added
+                      <br /> values, motivations and job match.
+                    </p>
+                    <p className="text-font PaddingTopForDescriptionTwo h5 fw-bold">
+                      Phone validation
+                    </p>
+                    <p className="text-font ">
+                      We’ll talk about key motivations, added
+                      <br /> values, motivations and job match.
+                    </p>
+                    <p className="text-font PaddingTopForDescription h5 fw-bold">
+                      Phone validation
+                    </p>
+                    <p className="text-font ">
+                      We’ll talk about key motivations, added
+                      <br /> values, motivations and job match.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </React.Fragment>
   );
