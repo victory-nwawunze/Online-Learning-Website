3b162aa053eb3153ab20fbeb6326f15bb29d519f
victory-nwawunze <victorynwawunze@gmail.com>
2022-06-10 13:53:53 -0700

    creating moneyAfrica subscribe page

---
+++ src/components/MoneyAfricaSubscribePage/MoneyAfricaSubscribePage.js
@@ -0,0 +1,24 @@
+/** @format */
+
+import React from "react";
+import MoneyAfricaLogo from "../../images/Group 8162.png";
+const MoneyAfricaSubscribePage = () => {
+  return (
+    <React.Fragment>
+      <div className="MoneyAfricaSubscribePage p-5">
+        <div className="text-center">
+          <img src={MoneyAfricaLogo} />
+          <p className="text-center pt-3 fw-bold text-font h2">
+            Choose your preferred plan
+          </p>
+          <p className="text-center text-font SignUpFormInputFontColor">
+            Choose a plan to procees and have access to all the
+            <br /> video and article courses on our platform.
+          </p>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default MoneyAfricaSubscribePage;
