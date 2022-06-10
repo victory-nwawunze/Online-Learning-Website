5b0a3bcad4071f9a69ed7ed62422f9017026b681
victory-nwawunze <victorynwawunze@gmail.com>
2022-06-10 12:19:05 -0700

    creating moneyAfrica Verification page

---
+++ src/components/MoneyAfricaSignUpVerification/MoneyAfricaSignUpVerification.js
@@ -0,0 +1,20 @@
+/** @format */
+
+import React from "react";
+import MoneyAfricaLogo from "../../images/Group 8162.png";
+const MoneyAfricaSignUpVerification = () => {
+  return (
+    <React.Fragment>
+      <div className="MoneyAfricaSignUpVerification p-5">
+        <div className="text-center">
+          <img src={MoneyAfricaLogo} />
+        </div>
+        <p className="text-font h3 fw-bold text-center pt-4">
+          Email Verification
+        </p>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default MoneyAfricaSignUpVerification;
