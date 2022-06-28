b9d8b49385c237a9323d26d855410b96d5301b3e
victory-nwawunze <victorynwawunze@gmail.com>
2022-06-28 04:27:19 -0700

    creating moneyAfrica ArticlePage

--- src/components/MoneyAfricaArticleListingPage/MoneyAfricaArticleListingPage.js
+++ src/components/MoneyAfricaArticleListingPage/MoneyAfricaArticleListingPage.js
@@ -1,11 +1,34 @@
 /** @format */
 
 import React from "react";
-
+import MoneyAfricaGreenLogo from "../../images/MoneyAfricaLogoGreen 1.png";
 const MoneyAfricaArticleListingPage = () => {
   return (
     <React.Fragment>
-      <h1>MoneyAfricaArticleListingPage</h1>
+      <div className="container">
+        <div className="d-flex align-items-center">
+          <div>
+            <p className="fw-bold text-font h3 mt-3">Learn with article</p>
+          </div>
+          <div>
+            <p className="text-font SmallTextFontPrice  mt-4 p-3">
+              Filter topics
+            </p>
+          </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="d-flex align-items-center">
+          <div>
+            <p className="fw-bold text-font h3 mt-3">Articles (6)</p>
+          </div>
+          <div>
+            <p className="text-font SmallTextFontPrice  mt-4 p-3">
+              Videos (14)
+            </p>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
