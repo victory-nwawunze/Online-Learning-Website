b438cf407d954f55cb672eddee3cbb310a00b273
victory-nwawunze <victorynwawunze@gmail.com>
2022-06-28 04:00:24 -0700

    creating moneyAfrica ArticlePage

---
+++ src/components/MoneyAfricaArticleListingPage/MoneyAfricaArticleListingPage.js
@@ -0,0 +1,13 @@
+/** @format */
+
+import React from "react";
+
+const MoneyAfricaArticleListingPage = () => {
+  return (
+    <React.Fragment>
+      <h1>MoneyAfricaArticleListingPage</h1>
+    </React.Fragment>
+  );
+};
+
+export default MoneyAfricaArticleListingPage;
