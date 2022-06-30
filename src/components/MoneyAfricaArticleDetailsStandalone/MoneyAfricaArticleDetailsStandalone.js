cbdadf25f86b3ed047e79d359030f18d4b717ca4
victory-nwawunze <victorynwawunze@gmail.com>
2022-06-30 04:16:18 -0700

    creating moneyAfrica ArticlePage

---
+++ src/components/MoneyAfricaArticleDetailsStandalone/MoneyAfricaArticleDetailsStandalone.js
@@ -0,0 +1,41 @@
+/** @format */
+
+import React from "react";
+
+const MoneyAfricaArticleDetailsStandalone = () => {
+  return (
+    <React.Fragment>
+      <div className="MoneyAfricaArticleStandAlone p-5 ">
+        <div className="container">
+          <div className="grid">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="d-flex">
+                  <div>1</div>
+                  <div>2</div>
+                  <div>3</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="moneyAfricaDescriptionText p-5">
+        <p className="text-font h2 text-center">
+          The Best Crypto Wallets for Binance
+          <br /> Smart Chain (BSC)
+        </p>
+        <p className="text-font SmallTextFont text-center">
+          Updated Aug 9, 2021
+        </p>
+        <div className="d-flex">
+          <div className="text-center">1</div>
+          <div>2</div>
+          <div>3</div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default MoneyAfricaArticleDetailsStandalone;
