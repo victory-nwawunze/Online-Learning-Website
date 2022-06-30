3c69e9340fbcae48b16a4bf7cb2ad2f3d56650ef
victory-nwawunze <victorynwawunze@gmail.com>
2022-06-30 04:44:43 -0700

    creating moneyAfrica ArticleDetailPageStandAlone

--- src/components/MoneyAfricaArticleDetailsStandalone/MoneyAfricaArticleDetailsStandalone.js
+++ src/components/MoneyAfricaArticleDetailsStandalone/MoneyAfricaArticleDetailsStandalone.js
@@ -48,6 +48,64 @@ const MoneyAfricaArticleDetailsStandalone = () => {
           </div>
         </div>
       </div>
+      <div className="MoneyAfricaDescriptionTextStandAlone p-3">
+        <div className="container">
+          <div className="grid">
+            <div className="row">
+              <div className="col-md-3">
+                <p className="text-font MoneyAfricaDescriptionTextStandAloneColor">
+                  The Best Crypto Wallets for Binance
+                  <br /> Smart Chain (BSC)
+                  <br />
+                  <span className="LoginTextColor">Introduction</span>
+                  <br /> What type of crypto wallet should I<br /> use?
+                </p>
+              </div>
+              <div className="col-md-6">
+                <p className="text-font fw-bold">Introduction</p>
+                <p className="text-font">
+                  Choosing a crypto wallet depends on your needs. Security,
+                  custodianship, and interacting with smart contracts are key
+                  points to consider. You also may want to access your wallet on
+                  different devices.
+                </p>
+                <p className="text-font mt-5">
+                  For browser extension wallets, MetaMask, MathWallet, and
+                  Binance Chain Wallet are good options. If you prefer a mobile
+                  experience, MetaMask, MathWallet, Trust Wallet and SafePal are
+                  available for iOS and Android. They’re decentralized and store
+                  your private key on your device. SafePal also offers a cold
+                  storage hardware wallet for anyone looking for the highest
+                  security. Apart from the hardware version, all crypto wallets
+                  mentioned are free to use.
+                </p>
+                <p className="fw-bold h5 text-font mt-5">
+                  What type of crypto wallet should I use?
+                </p>
+                <p className="text-font">
+                  Before you choose a specific wallet, think about what you
+                  need. There’s more than just one type of wallet out there.
+                  Most BSC wallets will be non-custodial, meaning you have
+                  control of your private key. BSC wallets are also typically
+                  hot wallets that allow you to quickly connect to DApps or
+                  start staking. Each type has its advantages and disadvantages.
+                </p>
+                <p className="text-font mt-5">
+                  1. Custodial - The wallet provider holds your private key.
+                  This is the case with your digital assets in an exchange’s
+                  wallet. Without owning your private key, you aren’t entirely
+                  in control of your wallet. Likely, you won’t be able to
+                  connect to DApps either. For example, if you only want to spot
+                  trade BSC tokens and other cryptoassets, a custodial wallet is
+                  a reasonable choice. But be careful. While this is safe to do
+                  on Binance, you should not trust your funds to any custodial
+                  wallet or exchange.
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   );
 };
