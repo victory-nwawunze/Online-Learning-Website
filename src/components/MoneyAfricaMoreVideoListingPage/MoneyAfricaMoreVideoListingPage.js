380c5e3049739e2866beaa3ac75d37c6df5745ce
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-04 04:28:25 -0700

    creating moneyAfrica MoreMoneyAfricaVideoListingPage

---
+++ src/components/MoneyAfricaMoreVideoListingPage/MoneyAfricaMoreVideoListingPage.js
@@ -0,0 +1,22 @@
+/** @format */
+
+import React from "react";
+
+const MoneyAfricaMoreVideoListingPage = () => {
+  return (
+    <React.Fragment>
+      <div className="moneyAfricaVideoList p-3">
+        <div className="container">
+          <div className="grid">
+            <div className="row">
+              <div className="col-md-6">1</div>
+              <div className="col-md-6">2</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </React.Fragment>
+  );
+};
+
+export default MoneyAfricaMoreVideoListingPage;
