20ac7dab20a6530056971b920899f62cc209c0af
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-04 04:51:45 -0700

    creating moneyAfrica MoreMoneyAfricaVideoListingPage

--- src/components/MoneyAfricaMoreVideoListingPage/MoneyAfricaMoreVideoListingPage.js
+++ src/components/MoneyAfricaMoreVideoListingPage/MoneyAfricaMoreVideoListingPage.js
@@ -16,7 +16,36 @@ const MoneyAfricaMoreVideoListingPage = () => {
                 <p className="text-font mt-2 m-4 fw-bold">Close Filter</p>
               </div>
             </div>
-            <p className="text-font mt-4">Filter by Topics</p>
+            <div className="container">
+              <p className="text-font mt-4">Filter by Topics</p>
+              <div className="d-flex">
+                <div className="m-1">MoneyAfrica</div>
+                <div className="m-1">BlockChain</div>
+                <div className="m-1">Etherium</div>
+                <div className="m-1">Tech</div>
+                <div className="m-1">History</div>
+                <div className="m-1">Wallet</div>
+              </div>
+              <div className="d-flex">
+                <div className="m-1">Consencus</div>
+                <div className="m-1">Cryptography</div>
+                <div className="m-1">Essentials</div>
+                <div className="m-1">Tutorial</div>
+                <div className="m-1">Use Cases</div>
+                <div className="m-1">Technical analysis</div>
+              </div>
+              <div className="d-flex">
+                <div className="m-1">Trading</div>
+                <div className="m-1">Mining</div>
+                <div className="m-1">Privacy</div>
+                <div className="m-1">Economics</div>
+                <div className="m-1">Security</div>
+                <div className="m-1">Cryptocurrency</div>
+              </div>
+              <div className="d-flex">
+                <div className="m-1">See all</div>
+              </div>
+            </div>
           </div>
         </div>
         {/* <div className="container">
