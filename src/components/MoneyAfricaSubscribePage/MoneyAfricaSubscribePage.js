f7c18c1fa2f8c475d5590adf1c82c12aa9d0d5c2
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-05 04:14:14 -0700

    creating moneyAfrica SelectASubscriptionPlan

--- src/components/MoneyAfricaSubscribePage/MoneyAfricaSubscribePage.js
+++ src/components/MoneyAfricaSubscribePage/MoneyAfricaSubscribePage.js
@@ -8,49 +8,18 @@ import MarkedCircle from "../../images/MarkedCircle.png";
 const MoneyAfricaSubscribePage = () => {
   return (
     <React.Fragment>
-      <div className="MoneyAfricaSubscribePage p-5">
-        <div className="text-center">
-          <img src={MoneyAfricaLogo} />
-          <p className="text-center pt-3 fw-bold text-font h2">
+      <div className="SubscriptionSelectAPlan p-3">
+        <div className="ChooseAPreferredPlayAndATimesIcon  p-3">
+          <p className="text-font text-center h2 ChoosePreferredTextFontSize fw-bold">
             Choose your preferred plan
           </p>
-          <p className="text-center text-font SignUpFormInputFontColor">
+          <p className="text-font text-center mt-3 ChoosePreferredPlanDescriptionTextFontSizeAndColor">
             Choose a plan to procees and have access to all the
             <br /> video and article courses on our platform.
           </p>
         </div>
-        <div className="d-flex justify-content-between ">
-          <div className="des-1 p-3 DescriptionillusterationBorderRadius">
-            <div className="description-Amount py-3">
-              <img src={DescriptionAmount} />
-            </div>
-            <div className="descriptionTextIllustration">
-              <div className="d-flex justify-content-between  align-items-center">
-                <div>
-                  <img src={MarkedCircle} />
-                </div>
-                <div>
-                  <p className="text-font px-4 ">Autorenews every 3 months.</p>
-                </div>
-              </div>
-              <div className="d-flex justify-content-between  align-items-center">
-                <div>
-                  <img src={MarkedCircle} />
-                </div>
-                <div>
-                  <p className="text-font px-4 ">
-                    Access to everything - video &<br /> article courses.
-                  </p>
-                </div>
-              </div>
-            </div>
-          </div>
-          <div className="des-2 p-2 ">
-            <p className="text-center p-3 text-font ReconmendedBackgroundColorText">
-              Recommended
-              <div className="ReconmendedBackgroundColorContent px-3"></div>
-            </p>
-          </div>
+        <div className="SelectASubcriptionPlanCards mt-4 p-3">
+          <h1>H1</h1>
         </div>
       </div>
     </React.Fragment>
