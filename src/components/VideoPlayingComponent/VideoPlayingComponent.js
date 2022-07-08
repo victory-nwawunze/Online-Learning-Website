ffda79cb7ad8a4218832dc8fecd928ff4740b08c
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-08 04:16:28 -0700

    creating moneyAfrica PlayDescriptionComponent

--- src/components/VideoPlayingComponent/VideoPlayingComponent.js
+++ src/components/VideoPlayingComponent/VideoPlayingComponent.js
@@ -1,13 +1,7 @@
 /** @format */
 
 import React from "react";
-import HomeIconImage from "../../images/HomeIconImage.png";
-import CheveronRightIcon from "../../images/ChevronRightImageIcon.png";
-import SmallPlayIconImage from "../../images/SmallPlayIconImage.png";
-import SmallLineImage from "../../images/SmallLine.png";
-import NextPlayIcon from "../../images/NextPlayIcon.png";
-import SmallShareIcon from "../../images/SmallShareIconImage.png";
-import SmallThreeDotedLine from "../../images/SmallThreeDotedIcon.png";
+
 import BlackMenuImage from "../../images/BlackMenuImage.png";
 import SmallPlayIcon from "../../images/SmallPlayIcon.png";
 import SmallRewindForwardIcon from "../../images/SmallPlayRewindForward.png";
@@ -15,65 +9,11 @@ import SmallRewindBackwardIcon from "../../images/SmallRewindBackwardIcon.png";
 import PlayLineImage from "../../images/PlayLineImage.png";
 import ZoomImageIcon from "../../images/ZoomIcon.png";
 import PlayVideoDiscriptionComponent from "../PlayVideoDescriptionComponent/PlayVideoDiscriptionComponent";
+import BreadCrumbComponent from "../BreadCrumbComponent/BreadCrumbComponent";
 const VideoPlayingComponent = () => {
   return (
     <React.Fragment>
-      <div className="BreadCrumbStyles p-3">
-        <div className="container-fluid">
-          <div className="d-flex justify-content-between align-items-center">
-            <div>
-              <div className="d-flex">
-                <div>
-                  <div className="HomeIconImageStyle mx-2">
-                    <img src={HomeIconImage} />
-                  </div>
-                </div>
-                <div>
-                  <div className="HomeIconImageStyle mx-2">
-                    <img src={CheveronRightIcon} />
-                  </div>
-                </div>
-                <div>
-                  <p className="VideosTextFont mx-2 mt-1">Videos</p>
-                </div>
-                <div>
-                  <p className="VideosTextFont mx-2 mt-1">
-                    The Best Crypto Wallets for Binance Smart Chain (BSC)
-                  </p>
-                </div>
-              </div>
-            </div>
-            <div>
-              <div className="d-flex">
-                <div className="mx-2 mt-1">
-                  <img src={SmallPlayIconImage} />
-                </div>
-                <div className="mx-1 PreviousTextFontStyle mt-1">
-                  <p>Previous</p>
-                </div>
-                <div className="mx-2">
-                  <img src={SmallLineImage} />
-                </div>
-                <div className="mx-1 NextTextFontStyle mt-1">
-                  <p>Next</p>
-                </div>
-                <div className="mx-1">
-                  <img src={NextPlayIcon} />
-                </div>
-                <div className="mx-2">
-                  <img src={SmallShareIcon} />
-                </div>
-                <div className="mx-1 ShareTextFontStyle mt-1">
-                  <p>Share</p>
-                </div>
-                <div className="mx-4">
-                  <img src={SmallThreeDotedLine} />
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
+      <BreadCrumbComponent />
       <div className="BackgroundBannerVideoImage">
         <div className="container-fluid">
           <div className="BlackMenuImage">
