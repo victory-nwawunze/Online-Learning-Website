f740a82e3a1e80c055e750b5e13a3230ce48f8fe
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-11 07:48:57 -0700

    creating moneyAfrica SettingPage

--- src/components/MoneyAfricaSettingsPage/MoneyAfricaSettingsPage.js
+++ src/components/MoneyAfricaSettingsPage/MoneyAfricaSettingsPage.js
@@ -6,6 +6,11 @@ import SmallForMoreIcon from "../../images/ForMore.png";
 import SmallNotificationIcon from "../../images/SmallNotificationIcon.png";
 import AVatarImage from "../../images/AvatarImage.png";
 import SmallArrowDown from "../../images/SmallArrowDown.png";
+import SmallRedUserIcon from "../../images/SmallRedUserIcon.png";
+import MobileSubscriptionIcon from "../../images/MobileSubscription.png";
+import GiftBagIcon from "../../images/GiftBag.png";
+import ChangePasswordIcon from "../../images/ChangePasswordIcon.png";
+import LogoutIcon from "../../images/LogoutIcon.png";
 const MoneyAfricaSettingsPage = () => {
   return (
     <React.Fragment>
@@ -57,6 +62,90 @@ const MoneyAfricaSettingsPage = () => {
         <div className="container">
           <h3 className="text-font TextMarginToTheLeft">My Settings</h3>
           <hr className="MarginForHorizontalRule" />
+        </div>
+      </div>
+      <div className="CreatingMoneyAfricaSettingFunctionalities mt-3 p-3">
+        <div className="container">
+          <div className="grid">
+            <div className="row">
+              <div className="col-md-3">
+                <div>
+                  <div className="ProfileTextBgColor">
+                    <div className="d-flex">
+                      <div className="PaddingForRedUserIconAndText">
+                        <img src={SmallRedUserIcon} />
+                      </div>
+                      <div className="PaddingForProfileAndText text-font mx-1">
+                        <p>My Profile</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div>
+                    <div className="PositioningTheSubscriptionIcon mt-2">
+                      <div className="d-flex">
+                        <div className="PaddingForMobileSubscriptionIconAndText">
+                          <img src={MobileSubscriptionIcon} />
+                        </div>
+                        <div className="SubscriptionTextWithIcon text-font mx-2">
+                          <p>Subscriptions</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <div className="d-flex">
+                    <div>
+                      <div className="PositioningTheSubscriptionIcon mt-2">
+                        <div className="d-flex">
+                          <div className="PaddingForMobileSubscriptionIconAndText">
+                            <img src={GiftBagIcon} />
+                          </div>
+                          <div className="SubscriptionTextWithIcon text-font mx-2">
+                            <p>Refer a friend</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <div className="d-flex">
+                    <div>
+                      <div className="PositioningTheSubscriptionIcon mt-2">
+                        <div className="d-flex">
+                          <div className="PaddingForMobileSubscriptionIconAndText">
+                            <img src={ChangePasswordIcon} />
+                          </div>
+                          <div className="SubscriptionTextWithIcon text-font mx-2">
+                            <p>Change password</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div>
+                  <div className="d-flex">
+                    <div>
+                      <div className="PositioningTheSubscriptionIcon mt-2">
+                        <div className="d-flex">
+                          <div className="PaddingForMobileSubscriptionIconAndText">
+                            <img src={LogoutIcon} />
+                          </div>
+                          <div className="SubscriptionTextWithIcon text-font mx-2">
+                            <p>Log out</p>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div className="col-md-3">1</div>
+              <div className="col-md-6">1</div>
+            </div>
+          </div>
         </div>
       </div>
     </React.Fragment>
