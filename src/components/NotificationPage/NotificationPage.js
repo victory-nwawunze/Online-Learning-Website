f47ed191a6907870076ccf06f41f17c370dabc18
victory-nwawunze <victorynwawunze@gmail.com>
2022-07-14 06:55:03 -0700

    creating moneyAfrica NotificationPage

--- src/components/NotificationPage/NotificationPage.js
+++ src/components/NotificationPage/NotificationPage.js
@@ -2,7 +2,8 @@
 
 import React from "react";
 import MoneyAfricaSettingsNavbarComponent from "../MoneyAfricaSettingsNavbarComponent/MoneyAfricaSettingsNavbarComponent";
-
+import NotificationBellIcon from "../../images/NotificationBellIcon.png";
+import SmallRedLeftIcon from "../../images/SmallRedLeftIcon.png";
 const NotificationPage = () => {
   return (
     <React.Fragment>
@@ -16,8 +17,31 @@ const NotificationPage = () => {
       <div className="MoneyAfricaNotificationPage">
         <div className="container">
           <div className="d-flex justify-content-between">
-            <div>1</div>
-            <div>2</div>
+            <div>
+              <div className="d-flex">
+                <div className="BorderColor">
+                  <img src={NotificationBellIcon} />
+                </div>
+                <div>
+                  <p className="NotificationText text-font">
+                    We just launched a new article course on{" "}
+                    <span className="TBStyle">“Treasury bills”</span>.
+                    <p>6 hours ago</p>
+                  </p>
+                </div>
+              </div>
+              <hr className="HRwidth" />
+            </div>
+            <div>
+              <div className="d-flex">
+                <div>
+                  <p className="text-font MarginGTCtext"> Go to course</p>
+                </div>
+                <div className="mt-2 mx-1">
+                  <img src={SmallRedLeftIcon} />
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
